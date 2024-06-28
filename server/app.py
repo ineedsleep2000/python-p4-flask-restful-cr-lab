0dbc188c93f9fbedeb4493eb3f260ce8e30aa080
Brian <turtle40965@gmail.com>
2024-06-27 23:42:31 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -16,11 +16,58 @@ db.init_app(app)
 
 api = Api(app)
 
+#must make our classes a resource for the api
 class Plants(Resource):
-    pass
+    #GET
+    def get(self):
+        #for each plant in Plant, turn to dictionary
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(jsonify(plants), 200)
+        #when we do a CRUD method, do this header
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+    #POST
+    def post(self):
+        #get json
+        data = request.get_json()
+        #a new plant has to have a name, image, and price
+        new_plant = Plant(
+            name=data["name"],
+            image=data["image"],
+            price=data["price"]
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+        #the new_plant now becomes a dictionary
+        new_plant_dict = new_plant.to_dict()
+
+        response = make_response(jsonify(new_plant_dict), 201)
+
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+#Route, sets Plants to the Route
+api.add_resource(Plants, "/plants")
+
 
 class PlantByID(Resource):
-    pass
+    def get(self, plant_id):
+        plant = Plant.query.filter_by(id=plant_id).first()
+
+        plant_dict = plant.to_dict()
+
+        response = make_response(jsonify(plant_dict), 200)
+
+        response.headers["Content-Type"] = "application/json"
+
+        return response
+
+
+api.add_resource(PlantByID, "/plants/<int:plant_id>")
         
 
 if __name__ == '__main__':
